8fd6b515dffcfe1a0fa415d5cbaeddc225ee281f
Renato de Oliveira Lucena <cpdrenato@gmail.com>
2021-05-04 11:38:12 -0300

    duplicates-Use On Laravel Collections

---
+++ app/Http/Controllers/CollectionController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
+
+class CollectionController extends Controller
+{
+    public function index()
+    {
+        $collection = collect(['renato', 'lisa', 'leidy', 'alexandre', 'renato', 'lisa', 'leidy', 'renato']);
+        $collection = $collection->duplicates();
+        echo($collection);
+    }   
+
+    public function teste3()
+    {
+        $collection = collect([
+            ["id"=>1, "name"=>"Renato", "role"=>"Admin"],
+            ["id"=>2, "name"=>"Leidy", "role"=>"Admin"],
+            ["id"=>3, "name"=>"Alexandre", "role"=>"User"]
+
+        ]);
+        $collection = $collection->duplicates('role');
+        echo($collection);
+
+    }
+
+    public function teste4()
+    {
+        $collection = collect([
+            ["id"=>1, "name"=>"Renato", "city"=>"Uberlandia", "country"=>'Brasil'],
+            ["id"=>2, "name"=>"Leidy", "city"=>"Goiania", "country"=>'Brasil'],
+            ["id"=>3, "name"=>"Alexandre", "city"=>"Uberlandia", "country"=>'Brasil'],
+            ["id"=>4, "name"=>"Hardik", "city"=>"Mumbai", "country"=>'India'],
+            ["id"=>5, "name"=>"Jhon", "city"=>"New York", "country"=>'US'],
+        ]);
+
+        $grouped = $collection->groupBy('country')->map(function($row){
+            //return response()->json($row->count(), 200);
+            //echo $row->count();
+            return $row->count();
+        });
+        echo($grouped);
+
+    }
+
+    public function teste2()
+    {
+        $collection = collect([
+            [
+                'name' => 'aa',
+                'value' => 22
+            ],
+            [
+                'name' => 'bb',
+                'value' => 22
+            ],
+            [
+                'name' => 'cc',
+                'value' => 11
+            ],
+            [
+                'name' => 'bb',
+                'value' => 33
+            ],
+            [
+                'name' => 'bb',
+                'value' => 33
+            ],
+            [
+                'name' => 'bb',
+                'value' => 33
+            ],
+        ]);
+
+        $groupedByValue = $collection->groupBy('value');
+        $dupes = $groupedByValue->filter(function(Collection $groups){
+            //echo ($groups->count() > 1);
+            return response()->json($groups->count() > 1, 200);
+            //return $groups->count() > 1;
+        });
+        echo($dupes);
+    }   
+
+    public function loop()
+    {
+       /* $item = [
+            'renato' => 30,
+            'leidy' => 23,
+            'alexandre' => 12
+        ];*/
+        
+        $collection = collect(['renato'  => 30, 'lisa'  => 31, 'leidy'  => 23, 'alexandre'  => 3, 'renato'  => 30, 'lisa'  => 31, 'leidy'  => 23, 'renato'  => 30]);
+        //$collection = $collection->duplicates();
+        $collection->each(function($item, $key) {
+            print_r($key);    
+        });
+        
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use App\Http\Controllers\CollectionController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('teste', 'CollectionController@index');
+Route::get('loop', 'CollectionController@loop');
+Route::get('teste2', 'CollectionController@teste2');
+Route::get('teste3', 'CollectionController@teste3');
+Route::get('teste4', 'CollectionController@teste4');
